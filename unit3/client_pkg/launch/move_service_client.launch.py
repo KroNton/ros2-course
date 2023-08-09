0b3f6fefb6f6411c377fdf9adcbe2c3ff97030e2
KroNton <kyrlosfekry@gmail.com>
2023-08-10 02:18:14 +0300

    add move client node & launch file

--- unit3/client_pkg/launch/move_service_client.launch.py
+++ unit3/client_pkg/launch/move_service_client.launch.py
@@ -6,6 +6,6 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='client_pkg',
-            executable='service_client_node',
+            executable='move_client_node',
             output='screen'),
     ])
